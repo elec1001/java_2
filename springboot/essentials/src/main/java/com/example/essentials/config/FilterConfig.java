466df60ccc1178c36ef22a5d708bc7091d166f4e
elec1001 <elec1001@gmail.com>
2025-02-27 11:25:57 +0900

    serrion

--- springboot/essentials/src/main/java/com/example/essentials/config/FilterConfig.java
+++ springboot/essentials/src/main/java/com/example/essentials/config/FilterConfig.java
@@ -1,4 +1,19 @@
 package com.example.essentials.config;
 
+import com.example.essentials.filter.LoggingFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FilterConfig {
+    @Bean
+    public FilterRegistrationBean<LoggingFilter> filterRegistrationBean() {
+        FilterRegistrationBean<LoggingFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+
+        filterRegistrationBean.setFilter(new LoggingFilter());
+        filterRegistrationBean.addUrlPatterns("/api/*");
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
 }
--- springboot/essentials/src/main/java/com/example/essentials/filter/LoggingFilter.java
+++ springboot/essentials/src/main/java/com/example/essentials/filter/LoggingFilter.java
@@ -1,4 +1,36 @@
 package com.example.essentials.filter;
 
-public class LoggingFilter {
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+//@Component //컨테이너가 관리하게 함
+public class LoggingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig); // 필터 초기화(필요시)
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        //**필수
+        HttpServletRequest request=(HttpServletRequest)servletRequest; // Http로 형번환
+        HttpServletResponse response=(HttpServletResponse)servletResponse;
+        //--->요청 정보 로깅
+        System.out.println("Request URI:"+request.getRequestURI());
+        System.out.println("Request Method:"+request.getMethod());
+        //필터 체인:계속해서 다음 필터 또는 서블릿으로 전달
+        filterChain.doFilter(servletRequest,servletResponse);
+        //<---응답 상태 코드 로깅
+        System.out.println("Response status:"+response.getStatus());//http 메서드
+    }
+    @Override
+    public void destroy() {
+        Filter.super.destroy();//필터 종료 시 처리(필요시)
+    }
 }
