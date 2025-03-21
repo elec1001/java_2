ab88ce1b7d333f5161519073bdf06c99bb02df94
elec1001 <elec1001@gmail.com>
2025-03-21 10:58:11 +0900

    edge service

---
+++ polar-service/edge-service/src/main/java/com/practice/spring/edgeservice/web/WebEndPoints.java
@@ -0,0 +1,40 @@
+package com.practice.spring.edgeservice.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.io.PipedOutputStream;
+
+@Configuration
+public class WebEndPoints {
+
+    @Bean
+    public RouterFunction<ServerResponse> routes() {
+        return RouterFunctions.route()
+                .GET(
+                        "/service-fallback",
+                        request->ServerResponse.ok().body(Mono.just(""),String.class)
+                )
+                .POST(
+                        "/service-fallback",
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build()
+                )
+                .PUT(
+                        "service-fallback",
+                        request -> ServerResponse.ok().body(Mono.just(""),String.class)
+                )
+                .DELETE(
+                        "service-fallback",
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).build()
+                )
+                .build();
+
+
+
+    }
+}
